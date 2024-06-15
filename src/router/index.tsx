3ef07d84f1175eeca1d8f604ad9cf501cdc5c2bc
zjx <2023654801@qq.com>
2024-06-15 14:41:57 +0800

    更新路由与登录界面

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom"
 import NotFoundPage from "@/components/404"
+import Login from "@/pages/Login"
 
 //路由配置
 const routes: RouteObject[] = [
   {
     path: "*",
     element: <NotFoundPage></NotFoundPage>
+  },
+  {
+    path: "/Login",
+    element: <Login></Login>
   }
 ]
 
