c67cc80e541dddc997bf47f6e0f8a3e2f3cc419e
mutojack <2973089938@qq.com>
2024-06-15 15:12:00 +0800

    更新了路由表，及路由初始组件或页面

---
+++ src/pages/GoodsEdit/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GoodsDetail: React.FC = () => {
+  return (
+    <>
+      <div>商品编辑</div>
+    </>
+  )
+}
+
+export default GoodsDetail
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,40 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
-import NotFoundPage from '@/components/404';
-import LayoutPage from '@/layout';
+import { createBrowserRouter, RouteObject } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import NotFoundPage from '@/components/404'
+import GoodsDetail from '@/pages/GoodsDetail'
+import LayoutPage from '@/layout'
+import GoodsList from '@/components/GoodsList'
 
 //路由配置
 const routes: RouteObject[] = [
-    {
+  {
+    path: '/admin',
+    element: <LayoutPage />,
+    children: [
+      {
+        path: '/list',
+        element: <GoodsList></GoodsList>
+      },
+      {
         path: '/',
-        element: <LayoutPage />,
-        children: []
-    },
-    {
-        path: '*',
-        element: <NotFoundPage></NotFoundPage>
-    }
-];
+        element: <Navigate to="/admin/list"></Navigate>
+      }
+    ],
+  },
+  {
+    path: '/detail/:id',
+    element: <GoodsDetail></GoodsDetail>
+  },
+  {
+    path: '*',
+    
+  },
+  {
+    path: '*',
+    element: <NotFoundPage></NotFoundPage>,
+  },
+]
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes)
 
-export default router;
+export default router
