ffeceab1914e0883883fb7a393a8c365aaa19ea6
zjx <2023654801@qq.com>
2024-06-15 15:40:19 +0800

    实现登录一部分

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,16 @@
+import "./index.css"
 function Login() {
     return <div>
-        <div></div>
-        <div></div>
+        <div className="Left_backgrand">
+            <div className="Left_content_background">
+
+            </div>
+        </div>
+        <div className="Right_backgrand">
+            <div className="Right_content_background">
+
+            </div>
+        </div>
     </div>
 }
 export default Login;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom"
 import NotFoundPage from "@/components/404"
+import LayoutPage from '@/layout';
 import Login from "@/pages/Login"
 
 //路由配置
 const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <LayoutPage />,
+    children: []
+  },
   {
     path: "*",
     element: <NotFoundPage></NotFoundPage>
