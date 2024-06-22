a63e2060af70e18da927e3d65e0b66fa105a7ccb
zjx <2023654801@qq.com>
2024-06-22 16:53:54 +0800

    优化路由守卫

--- src/router/routeGuard.tsx
+++ src/router/routeGuard.tsx
@@ -1,32 +1,42 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from 'react-redux';
-import { UserInfo } from '../store/userSlice'
+import { UserInfo } from '../store/userSlice';
+import { createSelector } from 'reselect'; // 引入 reselect
 
-interface RootState {
+// 定义 RootState 类型
+type RootState = {
     user: {
         userInfo: UserInfo | null;
     };
-}
+};
 
-const RouteGuard = (props: { children: JSX.Element }) => {
+// 创建一个 memoized selector 来获取 userInfo
+const selectUserInfo = createSelector(
+    (state: RootState) => state.user,
+    (user) => user.userInfo
+);
+
+const RouteGuard = ({ children }: { children: JSX.Element }) => {
     const WhiteList = ["/", "/login"];
-    const { pathname } = useLocation()
-    const userInfo = useSelector((state: RootState) => state);
+    const { pathname } = useLocation();
+    const userInfo = useSelector(selectUserInfo); // 使用更具体的选择器
 
-    if (WhiteList.indexOf((pathname ? pathname : "/")) === -1) {
-        if (userInfo.user.userInfo === null) {
-            return <Navigate to="/login" />;
-        } else if (userInfo.user.userInfo.roleName === "运营小二" || userInfo.user.userInfo.roleName === "超级管理员") {
-            if (userInfo.user.userInfo.roleName !== "超级管理员" && pathname === "/admin/platter") {
-                alert("您没有权限访问该页面")
-                return <Navigate to="/" />;
-            }
-        } else {
-            alert("请登录!")
-            return <Navigate to="/login" />;
-        }
+    // 检查路径是否在白名单中
+    if (WhiteList.includes(pathname)) {
+        return children;
     }
-    return props.children
-}
 
-export default RouteGuard
+    // 检查用户是否已登录以及是否有访问权限
+    if (!userInfo) {
+        return <Navigate to="/login" />;
+    }
+
+    if (userInfo.roleName !== "超级管理员" && pathname === "/admin/platter") {
+        alert("您没有权限访问该页面");
+        return <Navigate to="/" />;
+    }
+
+    return children;
+};
+
+export default RouteGuard;
