590167d4a1852c926b41e227d0da554575fef146
zjx <2023654801@qq.com>
2024-06-22 12:47:44 +0800

    路由守卫改v1.0

--- src/router/routeGuard.tsx
+++ src/router/routeGuard.tsx
@@ -14,8 +14,6 @@ const RouteGuard = (props: { children: JSX.Element }) => {
     const userInfo = useSelector((state: RootState) => state);
 
     if (WhiteList.indexOf((pathname ? pathname : "/")) === -1) {
-        console.log(pathname);
-        console.log(userInfo);
         if (userInfo.user.userInfo === null) {
             return <Navigate to="/login" />;
         } else if (userInfo.user.userInfo.roleName === "运营小二" || userInfo.user.userInfo.roleName === "超级管理员") {
